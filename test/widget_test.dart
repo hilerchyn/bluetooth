c01ffe93f6d77d49ab661199a18212144891da6a
chen tao <hilerchyn@gmail.com>
2021-05-16 08:44:19 +0800

    add demo

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bluetooth/main.dart';
+import 'package:bluetooth/main_bak.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
